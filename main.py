cdb4e2d602b503a71795ec7450ae63ab5911d00f
nathiboi <npayetteraynauld@hotmail.com>
2025-04-16 13:44:01 -0400

    player triangle

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
 from constants import * 
+from player import *
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    dt = 0
+    FPS = 60
+
+    screen.fill((0, 0, 0))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(pygame.Color(0, 0, 0))
+
+        player.draw(screen)
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(FPS) / 1000
 
 if __name__ == "__main__":
     main() 
