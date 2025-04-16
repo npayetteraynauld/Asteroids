6ab9abe8671917b3db8055dfa818e059c8e65fcc
nathiboi <npayetteraynauld@hotmail.com>
2025-04-16 14:33:46 -0400

    player move

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ def main():
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     dt = 0
-    FPS = 60
+    FPS = 180
 
-    screen.fill((0, 0, 0))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        screen.fill((0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
+        player.update(dt)
         dt = clock.tick(FPS) / 1000
 
 if __name__ == "__main__":
